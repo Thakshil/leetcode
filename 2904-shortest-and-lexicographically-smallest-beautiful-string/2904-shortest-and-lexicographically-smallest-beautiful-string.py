422ec2edfc974372e6e53e59e2e922848b7bb2a2
Eskala Thakshil <144157787+Thakshil@users.noreply.github.com>
2026-08-26 11:29:49 +0530

    Time: 16 ms (5.86%), Space: 19.7 MB (6.76%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.py
@@ -1,12 +1,15 @@
 class Solution:
     def shortestBeautifulSubstring(self, s: str, k: int) -> str:
         x=[]
+        y=[]
         for i in range(0,len(s)):
             for j in range(i+k,len(s)+1):
                 z=s[i:j]
-                if z.count("1")==k:
-                    x.append([len(z),z])
-        x.sort()
-        if x:
-            return x[0][1]
+                x.append(z)
+        for i in x:
+            if i.count("1")==k:
+                y.append([len(i),i])
+        y.sort()
+        if y:
+            return y[0][1]
         return ""
